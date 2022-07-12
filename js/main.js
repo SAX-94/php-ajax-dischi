6e8a3b4f26a37d9fd98b64a4c361507929bee151
SAX_94 <matteosacchi94@gmail.com>
2022-07-12 20:19:43 +0200

    failed trial to complete milestone 2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        db: []
+    },
+    mounted(){
+        axios.get("api.php")
+            .then(resp => {
+                this.db = resp.data.data
+                console.log(this.db)
+            })
+    }
+})
